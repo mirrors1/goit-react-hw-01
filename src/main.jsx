ce3bafcd9c526c3e2007329aff6d8808127b01be
Oleksandr Prokopchuk <sasha195@i.ua>
2025-01-10 21:31:40 +0200

    Add modern-normalize

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+// Імпорт стилів нормалізації
+import 'modern-normalize';
+import './index.css';
+import App from './components/App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
